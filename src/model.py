0e7d3538a848b69fa334c5bffb38adbccf0a5cac
dp-ops <dimos_pl@outlook.com>
2025-04-28 19:57:04 +0300

    Remove commented-out ImageCaptioningModel class and its methods for cleaner code

--- src/model.py
+++ src/model.py
@@ -497,38 +497,3 @@ class LSTMDecoderWithAttention(nn.Module):
             alphas[:batch_size_t, t, :] = alpha
 
         return predictions, encoded_captions, decode_lengths, alphas, sort_ind
-
-# # Example of creating a complete image captioning model
-# class ImageCaptioningModel(nn.Module):
-#     """
-#     Full image captioning model combining encoder and decoder
-#     """
-#     def __init__(self, encoder_dim=512, attention_dim=512, embed_dim=512, decoder_dim=512, 
-#                  vocab_size=10000, encoded_image_size=14, pretrained_path=None, fine_tune_encoder=False):
-#         super(ImageCaptioningModel, self).__init__()
-        
-#         # Initialize encoder and decoder
-#         self.encoder = EncoderCNN(encoded_image_size=encoded_image_size, 
-#                                   pretrained_path=pretrained_path, 
-#                                   fine_tune=fine_tune_encoder)
-        
-#         self.decoder = LSTMDecoderWithAttention(attention_dim=attention_dim,
-#                                                embed_dim=embed_dim,
-#                                                decoder_dim=decoder_dim,
-#                                                vocab_size=vocab_size,
-#                                                encoder_dim=encoder_dim)
-        
-#     def forward(self, images, encoded_captions, caption_lengths):
-#         """
-#         Forward propagation through the full model
-#         """
-#         # Encode the images
-#         encoder_out = self.encoder(images)
-        
-#         # Decode with attention
-#         predictions, encoded_captions, decode_lengths, alphas, sort_ind = self.decoder(
-#             encoder_out, encoded_captions, caption_lengths
-#         )
-        
-#         return predictions, alphas, encoded_captions, decode_lengths, sort_ind
-
